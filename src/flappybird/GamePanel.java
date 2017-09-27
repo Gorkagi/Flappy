1704f8c7b24bd32a197f6cab331b99bdfaa648e5
gorkagonzalo <gorka.gonzalo@alumni.mondragon.edu>
2017-09-27 21:34:02 +0200

    Fondo Cambiante por Tiempo

--- src/flappybird/GamePanel.java
+++ src/flappybird/GamePanel.java
@@ -29,7 +29,7 @@ public class GamePanel extends JPanel {
 	private Font scoreFont, pauseFont;
 	public static final Color bg = new Color(0, 158, 158);
 	public static final int PIPE_W = 50, PIPE_H = 30;
-	private Image pipeHead, pipeLength, background;
+	private Image pipeHead, pipeLength, background1, background2, background3;
 
 	public GamePanel(FlappyBird fb, Bird bird, ArrayList<Rectangle> rects) {
 		this.fb = fb;
@@ -41,7 +41,9 @@ public class GamePanel extends JPanel {
 		try {
 			pipeHead = ImageIO.read(new File("78px-Pipe.png"));
 			pipeLength = ImageIO.read(new File("pipe_part.png"));
-			background = ImageIO.read(new File("GameBack.png"));
+			background1 = ImageIO.read(new File("GameBack.png"));
+			background2 = ImageIO.read(new File("GameBack2.png"));
+			background3 = ImageIO.read(new File("GameBack3.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -49,7 +51,13 @@ public class GamePanel extends JPanel {
 
 	@Override
 	public void paintComponent(Graphics g) {
-		g.drawImage(background, 0, 0, 600, 600, null);
+		if(fb.getScore() < 150) {
+			g.drawImage(background1, 0, 0, 600, 600, null);
+		}else if(fb.getScore() > 150 && fb.getScore() < 250) {
+			g.drawImage(background2, 0, 0, 600, 600, null);
+		}else {
+			g.drawImage(background3, 0, 0, 600, 600, null);
+		}
 		bird.update(g);
 		g.setColor(Color.RED);
 		for (Rectangle r : rects) {
